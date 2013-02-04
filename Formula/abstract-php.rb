92606245fe43d78c19d5c03c21f612c1e26f11f0
Shon M <him@shonm.com>
2013-02-04 09:08:40 -0700

    Add with-pthreads, enables maintainer-zts/pthreads

--- Formula/abstract-php.rb
+++ Formula/abstract-php.rb
@@ -63,6 +63,7 @@ class AbstractPhp < Formula
     option 'with-intl', 'Include internationalization support'
     option 'with-imap', 'Include IMAP extension'
     option 'without-pear', 'Build without PEAR'
+    option 'with-pthreads', 'Build with ZTS and pthreads'
     option 'with-homebrew-openssl', 'Include OpenSSL support via Homebrew'
     option 'without-bz2', 'Build without bz2 support'
   end
@@ -282,6 +283,11 @@ INFO
 
     if build.include? 'without-pear'
       args << "--without-pear"
+    end
+
+    if build.include? 'with-pthreads'
+      args << "--enable-maintainer-zts"
+      args << "--enable-pthreads"
     end
 
     args
